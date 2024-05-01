d9a61103e3bf6811d98c35dc5e299767fb2f4640
tanay4343 <chourasiyatanay@gmail.com>
2024-05-01 15:37:36 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+class InstagramBot:
+    def __init__(self):
+        self.chrome_options = webdriver.ChromeOptions()
+        self.chrome_options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(options=self.chrome_options)
+        self.driver.get("https://www.instagram.com/.")
+
+    def login(self, username, password):
+        time.sleep(5)
+        username_input = self.driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input')
+        username_input.send_keys(username)
+
+        password_input = self.driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input')
+        password_input.send_keys(password)
+
+        login_button = self.driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button/div')
+        login_button.click()
+
+    def handle_popups(self):
+        time.sleep(2)
+        save_info_not_now = self.driver.find_element(By.CSS_SELECTOR,
+                                                     'div.x1i10hfl.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.xdl72j9.x2lah0s.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1q0g3np.x1lku1pv.x1a2a7pz.x6s0dn4.xjyslct.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x9f619.x1ypdohk.x1f6kntn.xwhw2v2.xl56j7k.x17ydfre.x2b8uid.xlyipyv.x87ps6o.x14atkfc.xcdnw81.x1i0vuye.xjbqb8w.xm3z3ea.x1x8b98j.x131883w.x16mih1h.x972fbf.xcfux6l.x1qhh985.xm0m39n.xt0psk2.xt7dq6l.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x1n5bzlp.x173jzuc.x1yc6y37[role="button"][tabindex="0"]')
+        save_info_not_now.click()
+        time.sleep(3)
+        notification_off_button = self.driver.find_element(By.CSS_SELECTOR,'button._a9--._ap36._a9_1[tabindex="0"]')
+        notification_off_button.click()
+
+
+if __name__ == "__main__":
+    bot = InstagramBot()
+    bot.login("taxia.i", "demo@1234")
+    time.sleep(5)
+    bot.handle_popups()
